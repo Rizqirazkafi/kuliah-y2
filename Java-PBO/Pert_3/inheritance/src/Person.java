41e53b4db4778a5f8c095a7b66e4ced16a864ea0
Rizqirazkafi <rizqirazkafi56@gmail.com>
2021-09-10 07:55:47 +0700

    pertemuan 3

---
+++ Java-PBO/Pert_3/inheritance/src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    protected String name;
+    protected String address;
+
+    /*
+    *
+    */
+
+    public Person(){
+        
+        System.out.println("Inside Person : Constructor");
+        name = ""; address="";
+    }
+
+}
