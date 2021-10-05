11f025d28158f9481f6c843e75388845f5402b92
Rizqirazkafi <rizqirazkafi56@gmail.com>
2021-10-05 12:15:33 +0700

    Menambahkan praktik TK

---
+++ TK/matematika1.py
@@ -0,0 +1,11 @@
+class matematika():
+    def rumus3(a,b,c):
+        x=a*6+b*8+c*8
+        print(x)
+        return x
+    def rumus4(a,b,c):
+        x=a*2+b*3+c*4
+        y=a*b*c
+        print(x,y)
+        return x,y
+
---
+++ TK/praktik15.py
@@ -0,0 +1,6 @@
+import matematika1
+from matematika1 import matematika
+matematika.rumus3(1,1,1)
+matematika.rumus4(3,3,3)
+
+
