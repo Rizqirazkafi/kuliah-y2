a6cef5b13630c85489534bfc910a9dbf9b9b97fc
Rizqirazkafi <rizqirazkafi56@gmail.com>
2021-12-08 16:44:42 +0700

    Added ActionEvent

--- Java-PBO/KasirGUIFromScratch/src/com/App.java
+++ Java-PBO/KasirGUIFromScratch/src/com/App.java
@@ -1,16 +1,17 @@
 package com;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.FontUIResource;
-
+import java.awt.event.*;
 import java.awt.Color;
 import java.awt.Font;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+public class App extends JFrame implements ActionListener{
+    App() {
         JLabel label = new JLabel();
         label.setText("Kasir Warung Makan");
-        label.setFont(new Font("Serif",Font.BOLD, 48));
+        label.setFont(new Font("Serif", Font.BOLD, 48));
         // label.setForeground(Color.WHITE);
         // label.setBackground(Color.GRAY);
         label.setOpaque(true);
@@ -19,97 +20,96 @@ public class App {
         // label.setBorder(new EmptyBorder(20,0,0,0));
         JPanel judul = new JPanel();
         // judul.setBackground(Color.GRAY);
-        judul.setBounds(0,0,1280,80);
+        judul.setBounds(0, 0, 1280, 80);
         judul.add(label);
         // Set menu
         JLabel menuText = new JLabel("Menu");
-        menuText.setBounds(20,0,300,20);
+        menuText.setBounds(20, 0, 300, 20);
         menuText.setFont(new Font("Serif", Font.PLAIN, 20));
         JComboBox menu = new JComboBox<>();
-        menu.setFont(new Font("Serif", Font.PLAIN ,30));
+        menu.setFont(new Font("Serif", Font.PLAIN, 30));
         menu.addItem("Paket 1 - 10k");
         menu.addItem("Paket 2 - 12k");
         menu.addItem("Paket 3 - 8k");
         menu.addItem("Paket 4 - 10k");
-        menu.setBounds(20,25,300,40);
+        menu.setBounds(20, 25, 300, 40);
         JButton menuKurang = new JButton("-");
-        menuKurang.setFont(new Font("Serif", Font.PLAIN,40));
-        menuKurang.setBounds(340,25,60,40);
+        menuKurang.setFont(new Font("Serif", Font.PLAIN, 40));
+        menuKurang.setBounds(340, 25, 60, 40);
         JButton menuTambah = new JButton("+");
-        menuTambah.setFont(new Font("Serif", Font.PLAIN,40));
-        menuTambah.setBounds(480,25,60,40);
-        JTextField jumlahMenu = new JTextField(); 
-        jumlahMenu.setBounds(410,25,60,40);
+        menuTambah.setFont(new Font("Serif", Font.PLAIN, 40));
+        menuTambah.setBounds(480, 25, 60, 40);
+        JTextField jumlahMenu = new JTextField();
+        jumlahMenu.setBounds(410, 25, 60, 40);
         JButton addBtn = new JButton("Add");
-        addBtn.setBounds(550,25,80,40);
-        addBtn.setFont(new Font("Serif", Font.PLAIN,20));
-        
+        addBtn.setBounds(550, 25, 80, 40);
+        addBtn.setFont(new Font("Serif", Font.PLAIN, 20));
+
         // Set menu tambahan
         JLabel tambahanTitle = new JLabel("Tambahan");
-        tambahanTitle.setBounds(20,70,300,20);
+        tambahanTitle.setBounds(20, 70, 300, 20);
         tambahanTitle.setFont(new Font("Serif", Font.PLAIN, 20));
         JComboBox tambahan = new JComboBox<>();
         tambahan.addItem("Nasi - 5k");
         tambahan.addItem("Es teh - 3k");
-        tambahan.setBounds(20,95,300,40);
-        tambahan.setFont(new Font("Serif", Font.PLAIN,30));
+        tambahan.setBounds(20, 95, 300, 40);
+        tambahan.setFont(new Font("Serif", Font.PLAIN, 30));
         JButton menuKurangTambahan = new JButton("-");
-        menuKurangTambahan.setFont(new Font("Serif", Font.PLAIN,40));
-        menuKurangTambahan.setBounds(340,95,60,40);
+        menuKurangTambahan.setFont(new Font("Serif", Font.PLAIN, 40));
+        menuKurangTambahan.setBounds(340, 95, 60, 40);
         JButton menuTambahTambahan = new JButton("+");
-        menuTambahTambahan.setFont(new Font("Serif", Font.PLAIN,40));
-        menuTambahTambahan.setBounds(480,95,60,40);
-        JTextField jumlahMenuTambahan = new JTextField(); 
-        jumlahMenuTambahan.setBounds(410,95,60,40);
+        menuTambahTambahan.setFont(new Font("Serif", Font.PLAIN, 40));
+        menuTambahTambahan.setBounds(480, 95, 60, 40);
+        JTextField jumlahMenuTambahan = new JTextField();
+        jumlahMenuTambahan.setBounds(410, 95, 60, 40);
         JButton addBtnTambahan = new JButton("Add");
-        addBtnTambahan.setBounds(550,95,80,40);
-        addBtnTambahan.setFont(new Font("Serif", Font.PLAIN,20));
-        // Text area total pesanan 
+        addBtnTambahan.setBounds(550, 95, 80, 40);
+        addBtnTambahan.setFont(new Font("Serif", Font.PLAIN, 20));
+        // Text area total pesanan
         JTextArea pesanan = new JTextArea();
         JScrollPane pesananScroll = new JScrollPane(pesanan);
         pesananScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        pesananScroll.setBounds(20,150,610,200);
+        pesananScroll.setBounds(20, 150, 610, 200);
 
         JTextField hargaTotal = new JTextField();
         hargaTotal.setText("Harga Total");
-        hargaTotal.setFont(new Font("Serif", Font.PLAIN,30));
-        hargaTotal.setBounds(20,370,455,40);
+        hargaTotal.setFont(new Font("Serif", Font.PLAIN, 30));
+        hargaTotal.setBounds(20, 370, 455, 40);
 
         JTextField outputHargaTotal = new JTextField();
-        outputHargaTotal.setBounds(485,370,145,40);
-        
+        outputHargaTotal.setBounds(485, 370, 145, 40);
+
         JTextField uang = new JTextField();
         uang.setText("Uang");
-        uang.setFont(new Font("Serif", Font.PLAIN,30));
-        uang.setBounds(20,420,455,40);
+        uang.setFont(new Font("Serif", Font.PLAIN, 30));
+        uang.setBounds(20, 420, 455, 40);
 
         JTextField inputUang = new JTextField();
-        inputUang.setBounds(485,420,145,40);
-       
+        inputUang.setBounds(485, 420, 145, 40);
+
         JButton bayar = new JButton("Bayar");
-        bayar.setBounds(485,470, 145,50);
-        bayar.setFont(new Font("Serif", Font.PLAIN,30));
+        bayar.setBounds(485, 470, 145, 50);
+        bayar.setFont(new Font("Serif", Font.PLAIN, 30));
 
         JTextField kembali = new JTextField();
         kembali.setText("Uang Kembalian");
-        kembali.setFont(new Font("Serif", Font.PLAIN,30));
-        kembali.setBounds(20,530,455,40);
+        kembali.setFont(new Font("Serif", Font.PLAIN, 30));
+        kembali.setBounds(20, 530, 455, 40);
 
         JTextField outputKembali = new JTextField();
-        outputKembali.setBounds(485,530,145,40);
+        outputKembali.setBounds(485, 530, 145, 40);
 
         JButton reset = new JButton();
         reset.setText("Reset");
-        reset.setFont(new Font("Serif", Font.PLAIN,30));
-        reset.setBounds(20,580,455,50);
+        reset.setFont(new Font("Serif", Font.PLAIN, 30));
+        reset.setBounds(20, 580, 455, 50);
 
         JButton Cetak = new JButton("Cetak");
-        Cetak.setBounds(485,580, 145,50);
-        Cetak.setFont(new Font("Serif", Font.PLAIN,30));
-
+        Cetak.setBounds(485, 580, 145, 50);
+        Cetak.setFont(new Font("Serif", Font.PLAIN, 30));
 
         JPanel kiri = new JPanel();
-        kiri.setBounds(0,81,640,640);
+        kiri.setBounds(0, 81, 640, 640);
         // kiri.setBackground(new Color(60,63,65));
         kiri.setLayout(null);
         kiri.add(menuText);
@@ -135,27 +135,27 @@ public class App {
         kiri.add(reset);
         kiri.add(Cetak);
 
-        JTextArea struk = new JTextArea(41,50);
+        JTextArea struk = new JTextArea(41, 50);
         struk.setEditable(false);
         JScrollPane strukOut = new JScrollPane(struk);
         strukOut.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        strukOut.setBounds(20,20,600,600);
+        strukOut.setBounds(20, 20, 600, 600);
 
         JPanel kanan = new JPanel();
-        kanan.setBounds(640,90,640,640); 
+        kanan.setBounds(640, 90, 640, 640);
         kanan.add(strukOut);
         // kanan.setBackground(Color.GREEN);
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(judul);
-        frame.add(kiri);
-        frame.add(kanan);
-        // frame.getContentPane().setBackground(Color.GRAY);
-        frame.setLayout(null);
-        frame.setResizable(false);
-        frame.setSize(1280,720);
-        frame.setVisible(true);
-        // frame.add(panelAtas);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(judul);
+        this.add(kiri);
+        this.add(kanan);
+        this.setLayout(null);
+        this.setResizable(false);
+        this.setSize(1280, 720);
+        this.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e){
+        
     }
 }
