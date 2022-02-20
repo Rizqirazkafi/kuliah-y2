9ffb2f67659a4c1a0f74582f84afc4ffea6ba0e8
Rizqirazkafi <rizqirazkafi56@gmail.com>
2022-02-20 19:55:04 +0700

    Added resize with no resample feature in convert.py

--- pcd/convert.py
+++ pcd/convert.py
@@ -1,5 +1,11 @@
 from PIL import Image
 from numpy import asarray
 image = Image.open('image.jpg')
-np_img = asarray(image)
+croppedIm = image.crop((0,0,5,5))
+croppedIm.show()
+croppedIm.save('croppedIm.jpg')
+width, height = croppedIm.size
+resizedCrIm=croppedIm.resize((width*80, height*80), resample=0)
+resizedCrIm.save('RCI.jpg')
+np_img = asarray(croppedIm)
 print(np_img)
