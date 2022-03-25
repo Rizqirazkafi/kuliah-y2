61da14a79bfef2ef69a9c8633f434133a26c35e4
Rizqirazkafi <rizqirazkafi56@gmail.com>
2022-03-25 11:19:29 +0700

    Added negative conversion

---
+++ pcd/pert4/tugas1/main.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as  np
+
+def openImg(img: str):
+    return cv2.imread(img)
+
+def showImg(img: np.ndarray, title: str = "image"):
+    cv2.imshow(title, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def negative(img: np.ndarray):
+    # make image negative
+    return 255 - img
+
+# main
+
+img = openImg("image/kson.jpg")
+img = negative(img)
+showImg(img)
